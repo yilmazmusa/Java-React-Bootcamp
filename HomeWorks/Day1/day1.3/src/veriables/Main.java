9d7dbc519ea9d8f3d66ff19c03471fb423e24b6d
Musa <yilmazmusa66@gmail.com>
2021-05-03 19:19:15 +0300

    update

---
+++ HomeWorks/Day1/day1.3/src/veriables/Main.java
@@ -0,0 +1,15 @@
+package veriables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int ogrenciSayisi=7;  //camel casing yani ilk harf küçük sonraki kelimenin ilk harfi büyük
+		
+		System.out.println("Öðrenci Sayi: " + ogrenciSayisi);
+
+	}
+
+}
