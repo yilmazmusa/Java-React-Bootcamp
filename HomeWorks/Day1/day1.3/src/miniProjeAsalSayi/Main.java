b1e23c787dbf64053b1c6a9a7ca54bdc31052ad2
Musa <yilmazmusa66@gmail.com>
2021-05-04 12:16:54 +0300

    update

--- HomeWorks/Day1/day1.3/src/miniProjeAsalSayi/Main.java
+++ HomeWorks/Day1/day1.3/src/miniProjeAsalSayi/Main.java
@@ -3,33 +3,44 @@ package miniProjeAsalSayi;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		int sayi=8;
-		int remainder =sayi % 2;
+
+		int sayi = 1;
+		int remainder = sayi % 2;
 		System.out.println(remainder);
-		
-		
-		boolean isPrime=true;
-		
-		for(int i=2;i<sayi;i++) {
-			
-			if(sayi % i ==0) {
-				isPrime=false;
-				
+
+		boolean isPrime = true;
+
+		if (sayi < 1) {
+
+			System.out.println("Gecersiz Deger");
+			return;
+
+		}
+
+		if (sayi == 1) {
+
+			System.out.println("Sayi asal degildir");
+			return;
+		}
+
+		for (int i = 2; i < sayi; i++) {
+
+			if (sayi % i == 0) {
+				isPrime = false;
+
 			}
-			
-			
+
 		}
-		
-		if(isPrime) {
-			
-			System.out.println(sayi+" asaldir");
-			
+
+		if (isPrime) {
+
+			System.out.println(sayi + " asaldir");
+
 		}
-		
+
 		else {
-			
-			System.out.println(sayi+" asal deðildir");
+
+			System.out.println(sayi + " asal degildir");
 		}
 	}
 
